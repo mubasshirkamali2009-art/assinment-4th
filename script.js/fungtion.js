8f1d756fe701fd601652681ce15b1f4c4dc03345
mubasshir <mubasshirkamali2009@gmail.com>
2026-02-22 20:42:07 +0600

    frist fungtion

--- script.js/fungtion.js
+++ script.js/fungtion.js
@@ -1,1 +1,192 @@
-console.log("all okey?")
+const statusbtn1  =document.getElementById("stetus-1")
+const  interViewBtn1=
+document.getElementById("interview-stetus-btn-1")
+const  rejectedbtn1=
+document.getElementById("rejected-stetus-btn-1")
+
+ 
+// interview condition 
+interViewBtn1.addEventListener("click",function(){
+    statusbtn1.innerText="INTERVIEW"
+    statusbtn1.classList.remove("text-red-500")
+    statusbtn1.classList.remove("bg-red-100")
+   
+    statusbtn1.classList.add("text-green-500")
+    statusbtn1.classList.add("bg-green-100")
+}
+)
+rejectedbtn1.addEventListener("click",function(){
+    statusbtn1.innerText="REJECTED"
+    statusbtn1.classList.remove("text-green-500")
+    statusbtn1.classList.remove("bg-green-100")
+      statusbtn1.classList.add("bg-red-100")
+    statusbtn1.classList.add("text-red-500")
+})
+const statusbtn2  =document.getElementById("stetus-2")
+const  interViewBtn2=
+document.getElementById("interview-stetus-btn-2")
+const  rejectedbtn2=
+document.getElementById("rejected-stetus-btn-2")
+
+ 
+// interview condition 
+interViewBtn2.addEventListener("click",function(){
+    statusbtn2.innerText="INTERVIEW"
+    statusbtn2.classList.remove("text-red-500")
+    statusbtn2.classList.remove("bg-red-100")
+   
+    statusbtn2.classList.add("text-green-500")
+    statusbtn2.classList.add("bg-green-100")
+}
+)
+rejectedbtn2.addEventListener("click",function(){
+    statusbtn2.innerText="REJECTED"
+    statusbtn2.classList.remove("text-green-500")
+    statusbtn2.classList.remove("bg-green-100")
+      statusbtn2.classList.add("bg-red-100")
+    statusbtn1.classList.add("text-red-500")
+})
+const statusbtn3  =document.getElementById("stetus-3")
+const  interViewBtn3=
+document.getElementById("interview-stetus-btn-3")
+const  rejectedbtn3=
+document.getElementById("rejected-stetus-btn-3")
+
+ 
+// interview condition 
+interViewBtn3.addEventListener("click",function(){
+    statusbtn3.innerText="INTERVIEW"
+    statusbtn3.classList.remove("text-red-500")
+    statusbtn3.classList.remove("bg-red-100")
+   
+    statusbtn3.classList.add("text-green-500")
+    statusbtn3.classList.add("bg-green-100")
+}
+)
+rejectedbtn3.addEventListener("click",function(){
+    statusbtn3.innerText="REJECTED"
+    statusbtn3.classList.remove("text-green-500")
+    statusbtn3.classList.remove("bg-green-100")
+      statusbtn3.classList.add("bg-red-100")
+    statusbtn3.classList.add("text-red-500")
+})
+const statusbtn4  =document.getElementById("stetus-4")
+const  interViewBtn4=
+document.getElementById("interview-stetus-btn-4")
+const  rejectedbtn4=
+document.getElementById("rejected-stetus-btn-4")
+
+ 
+// interview condition 
+interViewBtn4.addEventListener("click",function(){
+    statusbtn4.innerText="INTERVIEW"
+    statusbtn4.classList.remove("text-red-500")
+    statusbtn4.classList.remove("bg-red-100")
+   
+    statusbtn4.classList.add("text-green-500")
+    statusbtn4.classList.add("bg-green-100")
+}
+)
+rejectedbtn4.addEventListener("click",function(){
+    statusbtn4.innerText="REJECTED"
+    statusbtn4.classList.remove("text-green-500")
+    statusbtn4.classList.remove("bg-green-100")
+      statusbtn4.classList.add("bg-red-100")
+    statusbtn4.classList.add("text-red-500")
+})
+const statusbtn5  =document.getElementById("stetus-5")
+const  interViewBtn5=
+document.getElementById("interview-stetus-btn-5")
+const  rejectedbtn5=
+document.getElementById("rejected-stetus-btn-5")
+
+ 
+// interview condition 
+interViewBtn5.addEventListener("click",function(){
+    statusbtn5.innerText="INTERVIEW"
+    statusbtn5.classList.remove("text-red-500")
+    statusbtn5.classList.remove("bg-red-100")
+   
+    statusbtn5.classList.add("text-green-500")
+    statusbtn5.classList.add("bg-green-100")
+}
+)
+rejectedbtn5.addEventListener("click",function(){
+    statusbtn5.innerText="REJECTED"
+    statusbtn5.classList.remove("text-green-500")
+    statusbtn5.classList.remove("bg-green-100")
+      statusbtn5.classList.add("bg-red-100")
+    statusbtn5.classList.add("text-red-500")
+})
+const statusbtn6  =document.getElementById("stetus-6")
+const  interViewBtn6=
+document.getElementById("interview-stetus-btn-6")
+const  rejectedbtn6=
+document.getElementById("rejected-stetus-btn-6")
+
+ 
+// interview condition 
+interViewBtn6.addEventListener("click",function(){
+    statusbtn6.innerText="INTERVIEW"
+    statusbtn6.classList.remove("text-red-500")
+    statusbtn6.classList.remove("bg-red-100")
+   
+    statusbtn6.classList.add("text-green-500")
+    statusbtn6.classList.add("bg-green-100")
+}
+)
+rejectedbtn6.addEventListener("click",function(){
+    statusbtn6.innerText="REJECTED"
+    statusbtn6.classList.remove("text-green-500")
+    statusbtn6.classList.remove("bg-green-100")
+      statusbtn6.classList.add("bg-red-100")
+    statusbtn6.classList.add("text-red-500")
+})
+const statusbtn7  =document.getElementById("stetus-7")
+const  interViewBtn7=
+document.getElementById("interview-stetus-btn-7")
+const  rejectedbtn7=
+document.getElementById("rejected-stetus-btn-7")
+
+ 
+// interview condition 
+interViewBtn7.addEventListener("click",function(){
+    statusbtn7.innerText="INTERVIEW"
+    statusbtn7.classList.remove("text-red-500")
+    statusbtn7.classList.remove("bg-red-100")
+   
+    statusbtn7.classList.add("text-green-500")
+    statusbtn7.classList.add("bg-green-100")
+}
+)
+rejectedbtn7.addEventListener("click",function(){
+    statusbtn7.innerText="REJECTED"
+    statusbtn7.classList.remove("text-green-500")
+    statusbtn7.classList.remove("bg-green-100")
+      statusbtn7.classList.add("bg-red-100")
+    statusbtn7.classList.add("text-red-500")
+})
+const statusbtn8  =document.getElementById("stetus-8")
+const  interViewBtn8=
+document.getElementById("interview-stetus-btn-8")
+const  rejectedbtn8=
+document.getElementById("rejected-stetus-btn-8")
+
+ 
+// interview condition 
+interViewBtn8.addEventListener("click",function(){
+    statusbtn8.innerText="INTERVIEW"
+    statusbtn8.classList.remove("text-red-500")
+    statusbtn8.classList.remove("bg-red-100")
+   
+    statusbtn8.classList.add("text-green-500")
+    statusbtn8.classList.add("bg-green-100")
+}
+)
+rejectedbtn8.addEventListener("click",function(){
+    statusbtn8.innerText="REJECTED"
+    statusbtn8.classList.remove("text-green-500")
+    statusbtn8.classList.remove("bg-green-100")
+      statusbtn8.classList.add("bg-red-100")
+    statusbtn8.classList.add("text-red-500")
+})
